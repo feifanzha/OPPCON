945b6d11e9e6a3f31a313b7a18803f62a1ac192a
feifanzha <114387868+feifanzha@users.noreply.github.com>
2024-10-02 13:43:53 +0800

    Update optical.h
    some program error, about the initial param

--- optical.h
+++ optical.h
@@ -14,11 +14,11 @@ struct PlaneModel{//ax+by+cz+d = 0
 };
 std::vector<cv::Point2f> Get8Pix(const cv::Point2f &keypoints_1,std::vector<cv::Point2f> &pix_surround);
 std::vector<cv::Point2f> diff(const std::vector<cv::Point2f>& vec1,const std::vector<cv::Point2f> &vec2,const float camera_fx,const float camera_fy);
-std::vector<cv::Point2f> OFCal(const std::vector<cv::Point2f> &pixels,const cv::Mat &frame1, const cv::Mat &frame2,std::vector<uchar> &status,float camera_fx = 1.0F,float camera_fy = 1.0F);
+std::vector<cv::Point2f> OFCal(const std::vector<cv::Point2f> &pixels,const cv::Mat &frame1, const cv::Mat &frame2,std::vector<uchar> &status,float camera_fx,float camera_fy);
 float WeightSum(const std::vector<cv::Point2f> &neighbours ,const int pos, const std::vector<float> &weight,int locate);
 float GraudOp(const std::vector<cv::Point2f> &neighbors, const int pos);
 bool WhetherChase(const std::vector<cv::Point2f> &neighbors,const std::vector<uchar> & status, const int pos);
-std::vector<cv::Point3f> Pixel23D(std::vector<cv::Point2f> &keypoints_1,const cv::Mat &img1,const cv::Mat &img2,float camera_fx = 1.0F,float camera_fy = 1.0F);
+std::vector<cv::Point3f> Pixel23D(std::vector<cv::Point2f> &keypoints_1,const cv::Mat &img1,const cv::Mat &img2,float camera_fx,float camera_fy);
 double point2PlaneDistance(const cv::Point3f& point, const PlaneModel& plane);
 std::vector<cv::Point2f> estimatePlaneRansac(const std::vector<cv::Point3f> &points,int  max_iterations,double threshold);
 void estimatePlaneRansac(const std::vector<cv::Point3f> &points, std::vector<cv::Point2f> &static_kps,std::vector<cv::Point2f> &dynamic_kps,int  max_iterations,double threshold);
